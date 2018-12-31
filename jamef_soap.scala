a5404e5e0151b5318f7d66ac84f514c75e2b27ad
FilSantos <filsantos1984@gmail.com>
2018-12-31 16:05:35 -0200

    Update jamef_soap.scala

--- jamef_soap.scala
+++ jamef_soap.scala
@@ -23,7 +23,37 @@ val headers = Map(
     	.exec(http("Request") // Here's an example of a POST request
       	.post("/JAMW0520.apw")
       	.headers(headers)
-	.body(RawFileBody("consultafreteProtheus.txt"))
+	.body(RawFileBody("<soapenv:Envelope xmlns:soapenv='http://schemas.xmlsoap.org/soap/envelope/' xmlns:tot='http://totjobcl03.dtc.jamef:8088/'>
+   <soapenv:Header/>
+   <soapenv:Body>
+      <tot:JAMW0520_01>
+         <tot:TIPTRA>1</tot:TIPTRA>
+         <tot:CNPJCPF>05148230000190</tot:CNPJCPF>
+         <tot:MUNORI>MOGI DAS CRUZES</tot:MUNORI>
+         <tot:ESTORI>SP</tot:ESTORI>
+         <tot:MUNDES>BERTIOGA</tot:MUNDES>
+         <tot:ESTDES>SP</tot:ESTDES>
+         <tot:SEGPROD>1</tot:SEGPROD>
+         <tot:QTDVOL>1</tot:QTDVOL>
+         <tot:PESO>1</tot:PESO>
+         <tot:VALMER>1000</tot:VALMER>
+         <tot:METRO3>1</tot:METRO3>
+         <tot:CNPJREM>05148230000190</tot:CNPJREM>
+         <tot:CNPJDES>05148230000190</tot:CNPJDES>
+         <tot:CIFFOB>1</tot:CIFFOB>
+         <!--Optional:-->
+         <tot:FILCOT>?</tot:FILCOT>
+         <!--Optional:-->
+         <tot:GRVLOG>?</tot:GRVLOG>
+         <!--Optional:-->
+         <tot:SEQEND>?</tot:SEQEND>
+         <!--Optional:-->
+         <tot:ENTFIL>?</tot:ENTFIL>
+         <!--Optional:-->
+         <tot:CONTRIB>?</tot:CONTRIB>
+      </tot:JAMW0520_01>
+   </soapenv:Body>
+</soapenv:Envelope>"))
 
 )
 
